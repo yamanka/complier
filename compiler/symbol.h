9d4ffd78544d67ce145364944992c61041db3574
郑思挺 <yamanka@outlook.com>
2014-11-15 19:45:38 +0800

    first commit

---
+++ compiler/symbol.h
@@ -0,0 +1,35 @@
+#define nul	0
+#define ident	1
+#define number	2
+#define plus	3	//+
+#define minus	4	//-
+#define times	5	//*
+#define slash	6	// /
+#define eql	7	//==
+#define neq	8	//!=
+#define lss	9	//<
+#define leq	10	//<=
+#define gtr	11	//>
+#define geq	12	//>=
+#define lparen	13	//(
+#define rparen	14	//)
+#define comma	15	//,
+#define semicolon	16	//;
+#define period	17	//.
+#define becomes 18	//=
+#define lbrace	19	//{
+#define rbrace	20	//}
+#define ifsym	21	
+#define thensym	22
+#define elsesym	23
+#define whilesym	24
+#define constsym	25
+#define scanfsym	26
+#define printfsym	27
+#define returnsym	28
+#define intsym	29
+#define charsym	30
+#define switchsym	31
+#define casesym	32
+#define	voidsym	33
+#define mainsym 34
