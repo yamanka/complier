af91b43caaec5503ee466098069284906308a884
yamanka <yamanka@outlook.com>
2014-12-19 17:22:49 +0800

    12/19

--- compiler/x86code.asm
+++ compiler/x86code.asm
@@ -6,59 +6,60 @@ Include \masm32\include\windows.inc
 Include \masm32\include\masm32rt.inc
 IncludeLib \masm32\lib\msvcrt.lib
 .data
-a dword ?
-d dword 10 DUP(?)
+n dword 10 DUP(?)
 .code
 _search proc
 push ebp
 mov ebp,esp
-sub esp,4
+sub esp,0
 sub esp,12
 mov eax,1
-mov dword ptr [ebp-4-8],eax
+mov dword ptr [ebp-0-8],eax
 mov eax, dword ptr[ebp+8]
-mov dword ptr [ebp-4-4],eax
-mov eax,dword ptr[ebp-4-4]
-mov ecx,dword ptr[ebp-8-4]
+mov dword ptr [ebp-0-4],eax
+mov eax,dword ptr[ebp-4-0]
+mov ecx,dword ptr[ebp-8-0]
 cmp eax,ecx
 jne sig_1
 mov eax,1
-mov dword ptr [ebp-4-4],eax
+mov dword ptr [ebp-0-4],eax
 mov esp,ebp
 pop ebp
 ret 4
 jmp sig_2
-sig_1:sig_2:mov eax,1
-mov dword ptr [ebp-4-12],eax
+sig_1:mov eax,1
+mov dword ptr [ebp-0-12],eax
 mov eax, dword ptr[ebp+8]
-mov dword ptr [ebp-4-4],eax
-mov  eax, dword ptr[ebp - 4-4]
-mov  ecx, dword ptr[ebp - 4-12]
+mov dword ptr [ebp-0-4],eax
+mov  eax, dword ptr[ebp - 0-4]
+mov  ecx, dword ptr[ebp - 0-12]
 sub eax,ecx
-mov dword ptr [ebp-4-8],eax
-push dword ptr [ebp-4-8]
+mov dword ptr [ebp-0-8],eax
+push dword ptr [ebp-0-8]
 call _search
-mov dword ptr [ebp-4-8],eax
-mov  eax, dword ptr[ebp - 4-4]
-mov  ecx, dword ptr[ebp - 4-8]
+mov dword ptr [ebp-0-8],eax
+mov  eax, dword ptr[ebp - 0-4]
+mov  ecx, dword ptr[ebp - 0-8]
 imul ecx
-mov dword ptr[ebp-4],eax
+mov dword ptr [ebp-0-4],eax
 mov esp,ebp
+pop ebp
+ret 4
+sig_2:mov esp,ebp
 pop ebp
 ret 4
 _search endp
 _main proc
 push ebp
 mov ebp,esp
-sub esp,4
+sub esp,12
 sub esp,4
 mov eax,10
-mov dword ptr [ebp-4-4],eax
-push dword ptr [ebp-4-4]
+mov dword ptr [ebp-12-4],eax
+push dword ptr [ebp-12-4]
 call _search
-mov dword ptr [ebp-4-4],eax
-mov  eax, dword ptr[ebp - 4-4]
-mov dword ptr[ebp-4],eax
+mov dword ptr [ebp-12-4],eax
+invoke crt_printf, CTXT("%d"),dword ptr [ebp-4-12]
 mov esp,ebp
 pop ebp
 ret 0
